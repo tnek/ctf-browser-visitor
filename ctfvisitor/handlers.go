d75af6380ee6d19fc24dd6d5f59781859d66187f
Kent Ma <kentma@squareup.com>
2022-11-08 08:04:37 -0500

    Add closure

--- ctfvisitor/handlers.go
+++ ctfvisitor/handlers.go
@@ -12,6 +12,8 @@ type Handler func(ctx context.Context, wd selenium.WebDriver) error
 // VisitHandler is a generic wrapper around Get() that also installs cookies
 // first before visiting the page
 func CookieHandler(path string, cookies []*selenium.Cookie) Handler {
+	path := path
+	cookies := cookies
 	h := func(ctx context.Context, wd selenium.WebDriver) error {
 		for _, cookie := range cookies {
 			if err := wd.AddCookie(cookie); err != nil {
