16d9a5f4d8b96c96d582bf6fceacf0782ca2ace5
Kent Ma <kentma@squareup.com>
2022-11-08 09:41:10 -0500

    Attempt 1 get before putting cookies in

--- ctfvisitor/handlers.go
+++ ctfvisitor/handlers.go
@@ -14,6 +14,7 @@ type Handler func(ctx context.Context, wd selenium.WebDriver) error
 func CookieHandler(
 	ctx context.Context, wd selenium.WebDriver,
 	path string, cookies []*selenium.Cookie) error {
+	wd.Get(path)
 	log.Printf("visiting %v with cookies %v\n", path, cookies)
 	for _, cookie := range cookies {
 		if err := wd.AddCookie(cookie); err != nil {
